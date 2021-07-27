29c761acbadd786569988e38366b115cb8f44b18
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-07-27 21:47:14 +0530

    Update and rename One and Zero Detector/one_detector.v to Ones and Zeros Detector 8 bit/one_detector.v

--- Ones and Zeros Detector 8 bit/one_detector.v
+++ Ones and Zeros Detector 8 bit/one_detector.v
@@ -1,32 +1,29 @@
-
-
-module zero_det(i,y
+module one_det(i,y
     );
-
 
 	 input wire [7:0]i;
 	 output y;
 
 	 wire w1,w2,w3,w4,w5,w6;
 
-	 and n1(w1,~i[0],~i[1]);
-	 and n2(w2,~i[2],~i[3]);
-	 and n3(w3,~i[4],~i[5]);
-	 and n4(w4,~i[6],~i[7]);
-	 and a1(w5,w1,w2);
-	 and a2(w6,w3,w4);
+	 nand n1(w1,i[0],i[1]);
+	 nand n2(w2,i[2],i[3]);
+	 nand n3(w3,i[4],i[5]);
+	 nand n4(w4,i[6],i[7]);
+	 and a1(w5,~w1,~w2);
+	 and a2(w6,~w3,~w4);
 	 and a3(y,w5,w6);
 
 endmodule
 
-module zero_detector_tb;
+module one_detector_tb;
 
 	// Inputs
 	reg [0:7]i;
 	wire y;
 
 	// Instantiate the Unit Under Test (UUT)
-	zero_det uut (
+	one_det uut (
 		i,y
 	);
 
@@ -35,10 +32,10 @@ module zero_detector_tb;
 		i = 255;#100;
 		i = 235;#100;
 		i = 0;#100;
-		i = 001;
+		i = 1;
 
 		// Wait 100 ns for global reset to finish
-		#100;$finish;
+		#100;
 
 		// Add stimulus here
 
