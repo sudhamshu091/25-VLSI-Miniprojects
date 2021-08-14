a13d587025618d02d464beaccfb8b97a92d0b1ce
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-08-14 20:02:49 +0530

    Create right_funnel_shifter_tb.v

---
+++ Funnel Shifter 10 bit/right_funnel_shifter_tb.v
@@ -0,0 +1,31 @@
+module rightt_funnel_shifter_tb;
+reg [9:0] in1,in2;
+reg [3:0] amt;
+wire [9:0] y;
+
+left_funnel_shifter right(in1,in2,amt,y);
+initial begin
+
+in1 = 10'b0000000001;
+in2 = 10'b0000000010;
+amt = 4'b0001;
+#20
+in1 = 10'b0000000001;
+in2 = 10'b0000000010;
+amt = 4'b0001;
+#20
+in1 = 10'b0000000001;
+in2 = 10'b0000000001;
+amt = 4'b0001;
+#20
+in1 = 10'b1000000001;
+in2 = 10'b1000000001;
+amt = 4'b0010;
+#20
+in1 = 10'b1000000001;
+in2 = 10'b1000000001;
+amt = 4'b0001;
+#20;
+  
+end
+endmodule
